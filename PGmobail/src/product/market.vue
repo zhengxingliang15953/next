673cd03168ef91ce8c349b58c45039e29a8c3e32
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-05-11 10:45:45 +0800

    zxl

---
+++ PGmobail/src/product/market.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div class="main-title">
+      营销推广
+      <div class="en-main-title">MARKETING</div>
+    </div>
+    <div class="panel-img-box">
+        <div class="panel-img-box-item">
+            <img src="../assets/img/weixin2.png"  alt="">
+            <h6>微信</h6>
+        </div>
+        <div class="panel-img-box-item">
+            <img src="../assets/img/douyin2.png"  alt="">
+            <h6>抖音</h6>
+        </div>
+        <div class="panel-img-box-item">
+            <img src="../assets/img/ruanwen1.png"  alt="">
+            <h6>软文</h6>
+        </div>
+        <div class="panel-img-box-item">
+            <img src="../assets/img/shipin1.png"  alt="">
+            <h6>视频</h6>
+        </div>
+    </div>
+    <h1>我们能做什么？</h1>
+    <h2>完整的全网营销系列产品，多方向助力网络营销</h2>
+    <div class="panel-market-box">
+        <div class="panel-market-box-top">
+            <div class="panel-market-box-top-left">
+                <img src="../assets/img/weixin3.png" width="100%" alt="">
+            </div>
+            <div class="panel-market-box-top-right">
+                <h5>微信朋友圈推广</h5>
+                <h6>做有效的推广</h6>
+            </div>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+img{
+    margin-bottom:10px;
+}
+.panel-img-box{
+    display: -webkit-flex;
+    display: flex;
+    justify-content:space-evenly;
+    .panel-img-box-item{
+        width:16%;
+        img{
+            width: 100%;
+            border-radius: 50%;
+        }
+        h6{
+            text-align: center;
+            margin-top:-10px;
+            font-size: 0.24rem;
+        }
+    }
+}
+h1{
+    text-align: center;
+    color: #333;
+    margin-top:20px;
+    font-size: 0.5rem;
+}
+h2{
+    width: 70%;
+    margin: 20px auto;
+    text-align: center;
+    font-weight: 400;
+}
+.panel-market-box{
+    width: 90%;
+    margin:0 auto;
+    border: 1px solid #e6e6e6;
+    .panel-market-box-top{
+        display: flex;
+        background: #1a54c8;
+        width: 100%;
+        color: white;
+        padding-left:10px;
+        .panel-market-box-top-left{
+            display: flex;
+            align-items: center;
+            width: 20%;
+            color: white;
+        }
+        .panel-market-box-top-right{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            color: white;
+            margin-left:10px;
+            h5{
+                color: white;
+                font-size: 0.35rem;
+            }
+            h6{
+                color: white;
+                font-size: 0.27rem;
+            }
+        }
+    }
+}
+</style>
--- PGmobail/src/views2/product.vue
+++ PGmobail/src/views2/product.vue
@@ -4,7 +4,7 @@
       产品中心
       <div class="en-main-title">POSITION</div>
     </div>-->
-    <van-tabs v-model="active" swipeable animated color="#0254ba">
+    <van-tabs swipeable animated color="#0254ba">
       <van-tab title="企业上网">
           <web></web>
       </van-tab>
@@ -14,7 +14,9 @@
       <van-tab title="盘古码">
           <pgma></pgma>
       </van-tab>
-      <van-tab title="营销推广">内容4</van-tab>
+      <van-tab title="营销推广">
+        <market></market>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -23,12 +25,13 @@
 import web from '../product/web';
 import pgma from '../product/pgma';
 import application from '../product/application';
+import market from '../product/market';
 export default {
   data() {
     return {};
   },
   components:{
-      web,pgma,application
+      web,pgma,application,market
   }
 };
 </script>
