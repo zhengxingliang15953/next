0932f40813ff3ffb0806d6959a700fe69ef7645b
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-05-12 16:58:45 +0800

    zxl

--- PGmobail/src/views2/product.vue
+++ PGmobail/src/views2/product.vue
@@ -4,34 +4,53 @@
       产品中心
       <div class="en-main-title">POSITION</div>
     </div>
-    <van-tabs swipeable animated color="#0254ba">
-      <van-tab title="企业上网">
+    <div ref="selectLi">
+      <van-tabs swipeable animated color="#0254ba">
+        <van-tab title="企业上网">
           <web></web>
-      </van-tab>
-      <van-tab title="企业应用">
+        </van-tab>
+        <van-tab title="企业应用">
           <application></application>
-      </van-tab>
-      <van-tab title="盘古码">
+        </van-tab>
+        <van-tab title="盘古码">
           <pgma></pgma>
-      </van-tab>
-      <van-tab title="营销推广">
-        <market></market>
-      </van-tab>
-    </van-tabs>
+        </van-tab>
+        <van-tab title="营销推广">
+          <market></market>
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
 <script>
-import web from '../product/web';
-import pgma from '../product/pgma';
-import application from '../product/application';
-import market from '../product/market';
+import web from "../product/web";
+import pgma from "../product/pgma";
+import application from "../product/application";
+import market from "../product/market";
 export default {
   data() {
     return {};
   },
-  components:{
-      web,pgma,application,market
+  components: {
+    web,
+    pgma,
+    application,
+    market
+  },
+  mounted() {
+    // window.addEventListener("scroll", this.scrollMethods);
+  },
+  methods: {
+    /* scrollMethods() {
+      let top = document.documentElement.scrollTop;
+      let tabs = this.$refs.selectLi;
+      if(tabs.getBoundingClientRect().top<=0){
+        tabs.classList.add('tabs-header');
+      }else{
+        tabs.classList.remove('tabs-header');
+      }
+    } */
   }
 };
 </script>
@@ -52,5 +71,10 @@ export default {
   padding: 5px 10px;
   text-align: center;
   background-color: #d2d0d0;
+}
+.tabs-header{
+  position: fixed;
+  top:0;
+  left:0;
 }
 </style>
