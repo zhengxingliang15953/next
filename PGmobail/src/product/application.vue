1a38ed89ac6586a1efda6ba16b3d4571255b3519
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-05-14 10:52:00 +0800

    zxl

--- PGmobail/src/product/application.vue
+++ PGmobail/src/product/application.vue
@@ -1,49 +1,5 @@
 <template>
   <div>
-    <!-- <div class="main-title">
-      企业应用
-      <div class="en-main-title">APPLICATION</div>
-    </div>-->
-    <!-- <div class="panel-two-title" style="margin-top:10px;">
-      <div class="panel-two-title-left">1</div>
-      <div class="panel-two-title-right">日常管理应用</div>
-    </div>
-    <div class="panel-body-text">
-         随着互联网+ 的推动，现在政府、事业单位、企业以及其他组织在各自的管理领域逐步转型，向电子化互联网化转型，使互联网+管理应用不断深化、触及面不断扩展，在行政审批、日常考勤、合同管理、电话会议、客户管理、销售员跟踪管理、业务跟单管理等方面得到充分应用。
-    </div>
-    <div class="application-web-img">
-        <div class="application-web-img-item">
-            <img src="../assets/img/zufang.jpg" height="100%" alt="">
-            <p>租房管理系统</p>
-        </div>
-        <div class="application-web-img-item">
-            <img src="../assets/img/kechuang.jpg" height="100%" alt="">
-            <p>科创中心OA系统</p>
-        </div>
-    </div>
-    <div class="panel-two-title">
-      <div class="panel-two-title-left">2</div>
-      <div class="panel-two-title-right">二维码应用</div>
-    </div>
-    <div class="panel-body-text">
-         产品二维码也就是一物一码概念，作为移动端产品信息查询接口，越来越多的使用在产品标签和产品包装上。通过产品二维码可以实现产品信息查询、产品防伪查询、产品保修以及产品追溯等功能，甚至有些企业通过产品二维码进行商业推广活动，让自己的产品成为宣传的载体。
-    </div>
-    <div class="application-web-img">
-        <div class="application-web-img-item">
-            <img src="../assets/img/dehong3.jpg" width="100%" height="100%" alt="">
-        </div>
-    </div>
-    <div class="panel-two-title">
-      <div class="panel-two-title-left">3</div>
-      <div class="panel-two-title-right">B2C业务应用</div>
-    </div>
-    <div class="panel-body-text">
-         "新华e菜场"小程序以客户、商品、订单为核心的前后端一体化的线上生鲜交易的小程序，客户可以实时维护客户订单，安排订单发货，对订单进行全程跟踪，删除订单，添加商品，财务管理，帮助企业快速响应供应链的需求。支持线上订单与门店的整合，顾客线上下单，线下自提或门店送货上门。
-    </div>
-    <div class="panel-two-title">
-      <div class="panel-two-title-left">4</div>
-      <div class="panel-two-title-right">网络安全应用</div>
-    </div>-->
     <div class="panel-application-container">
       <div class="panel-applicaion-title">
         日常管理应用
@@ -60,7 +16,7 @@
       </div>
       <img src="../assets/img/twoma.jpg" width="100%" alt="">
       <h3>产品的载体</h3>
-      <h1>一物一码 宣传产品</h1>
+      <h1>一物一码 管理宣传</h1>
       <hr></hr>
       <p>产品二维码也就是一物一码概念，作为移动端产品信息查询接口，越来越多的使用在产品标签和产品包装上。通过产品二维码可以实现产品信息查询、产品防伪查询、产品保修以及产品追溯等功能，甚至有些企业通过产品二维码进行商业推广活动，让自己的产品成为宣传的载体。</p>
     </div>
@@ -72,7 +28,7 @@
       <h3>应用不断深化</h3>
       <h1>打造掌上交易平台</h1>
       <hr></hr>
-      <p>“新华e菜场”小程序以客户、商品、订单为核心的前后端一体化的线上生鲜交易小程序，客户可以实时维护客户订单，安排订单发货，对订单进行全程跟踪，删除订单，添加商品，审核订单，财务管理，帮助企业快速响应供应链的需求。</p>
+      <p>我们能为您提供公众号、小程序和APP的线上综合服务平台,并与线上综合服务平台进行数据对接,根据监管要求,向线上综合服务平台发送订单、支付单、运单、个人物品申报单等数据。</p>
     </div>
     <div class="panel-application-container">
       <div class="panel-applicaion-title">
--- PGmobail/src/product/web.vue
+++ PGmobail/src/product/web.vue
@@ -24,10 +24,10 @@
       </ul>
     </div>
     <div class="panel-web-box" style="background-color:#f2f2f2;">
-      <h1>商标注册</h1>
+      <h1>企业邮箱</h1>
       <hr></hr>
-      <h3>为什么选择盘古注册商标？</h3>
-      <img src="../assets/img/trademark.png" width="112%" style="margin-left:-6%;margin-bottom:60px;" alt="">
+      <h3>为邮件沟通提供专属通道</h3>
+      <img src="../assets/img/web-email.png" width="100%" alt="">
     </div>
     <div class="panel-web-box">
       <h1>网站建设</h1>
@@ -35,7 +35,12 @@
       <h3>网站建设+品牌设计</h3>
       <h6>企业网站是企业在互联网上进行网络营销和形象宣传的平台，相当于企业的网络名片，不但对企业的形象是一个良好的宣传，同时可以辅助企业的销售，通过网络直接帮助企业实现产品的销售，企业可以利用网站来进行宣传、产品资讯发布、招聘等。</h6>
       <img src="../assets/img/webbuild.jpg" width="100%" alt="">
-  
+    </div>
+    <div class="panel-web-box" style="background-color:#f2f2f2;">
+      <h1>商标注册</h1>
+      <hr></hr>
+      <h3>全面支持商标申请线上提交</h3>
+      <img src="../assets/img/trademark.png" width="112%" style="margin-left:-6%;margin-bottom:60px;" alt="">
     </div>
   </div>
 </template>
